763b4b97341c209ff62305e51e1dec8630cff7dd
Guto Xavier <gutoffline@gmail.com>
2019-04-24 22:22:49 -0300

    Criação do jogador

---
+++ Arkanoid/Assets/Scripts/Jogador.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    public float velocidade = 10.0f;
+    public float horizontalAxis;
+    void Update()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalAxis * velocidade, 0);
+    }
+}
