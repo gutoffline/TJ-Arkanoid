b2ce4847e005ca730e8a6883db0d0bb6a67eeb1f
Guto Xavier <gutoffline@gmail.com>
2019-04-26 20:11:58 -0300

    Finalização do Jogo Arkanoid

--- Arkanoid/Assets/Scripts/Destruidor.cs
+++ Arkanoid/Assets/Scripts/Destruidor.cs
@@ -10,4 +10,8 @@ public class Destruidor : MonoBehaviour
         GameObject collider = collision.collider.gameObject;
         GameObject.Destroy(collider);
     }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        GameObject.Destroy(collider.gameObject);
+    }
 }
