4ad09ae12d351e8601839720a2023ed40621d4e0
Guto Xavier <gutoffline@gmail.com>
2019-04-26 19:54:16 -0300

    Criação dos blocos

---
+++ Arkanoid/Assets/Scripts/Destruidor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruidor : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject collider = collision.collider.gameObject;
+        GameObject.Destroy(collider);
+    }
+}
